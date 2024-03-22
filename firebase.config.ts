ddd577c766c911daa98bae2a9255303fd5ef81d5
dancreightondev <22957063+dancreightondev@users.noreply.github.com>
2024-03-22 11:50:26 +0000

    Set up Firebase

---
+++ firebase.config.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.FIREBASE_MSG_SENDER_ID,
+    appId: import.meta.env.FIREBASE_APP_ID,
+};
+
+// Initialise Firebase app
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialise Firebase auth
+export const firebaseAuth = getAuth(firebaseApp);
+
+// Export Firebase app instance
+export default firebaseApp;
